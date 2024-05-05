9085428807d9cab79036dcc0bb399dbd807f9f0c
hadi djemel <hadidjemel2@gmail.com>
2024-05-05 09:46:33 +0100

    05/05/2024- working on front end

---
+++ models/Post.js
@@ -0,0 +1,24 @@
+// models/Post.js
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+});
+
+module.exports = mongoose.model('Post', postSchema);
